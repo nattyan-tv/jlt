f4cde1b29422614ea48d915651485070e025c70a
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2021-03-17 19:16:46 +0900

    Novel-DEV

---
+++ jack_test.js
@@ -0,0 +1,140 @@
+﻿function next(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "あれ？";
+	botan.innerHTML = "<button type=button onclick='s1()'>▼</button>";
+}
+function s1(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "ん？　どうしたの？";
+	botan.innerHTML = "<button type=button onclick='s2()'>▼</button>";
+}
+function s2(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "ここどこ？";
+	botan.innerHTML = "<button type=button onclick='s3()'>▼</button>";
+}
+function s3(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "僕たちが簡易的なゲームになったんだよ！";
+	botan.innerHTML = "<button type=button onclick='s4()'>▼</button>";
+}
+function s4(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "こんなんがゲームなの？";
+	botan.innerHTML = "<button type=button onclick='s5()'>▼</button>";
+}
+function s5(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "まぁ、プログラム自体はとても簡単で、すぐに作れちゃうけど、意外と良い出来してるんだよ？";
+	botan.innerHTML = "<button type=button onclick='s6()'>▼</button>";
+}
+function s6(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "デザインとかも頑張れば、もっとゲームっぽくなるかもね！";
+	botan.innerHTML = "<button type=button onclick='s7()'>▼</button>";
+}
+function s7(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "じゃあジャック作ってよ！";
+	botan.innerHTML = "<button type=button onclick='s8()'>▼</button>";
+}
+function s8(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "えっ、俺出来ないよ！？";
+	botan.innerHTML = "<button type=button onclick='s9()'>▼</button>";
+}
+function s9(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "簡単だよ！";
+	botan.innerHTML = "<button type=button onclick='s10()'>▼</button>";
+}
+function s10(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "とりあえず、お金は出すからHTMLとJSとCSSの勉強してきて！";
+	botan.innerHTML = "<button type=button onclick='s11()'>▼</button>";
+}
+function s11(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "何言ってるのか分からないよ...";
+	botan.innerHTML = "<button type=button onclick='s12()'>▼</button>";
+}
+function s12(){
+	chara.innerHTML = "ひまわりたん";
+	document.mes.message.value = "あ、ジャック君！　丁度いいところにいた！";
+	botan.innerHTML = "<button type=button onclick='s13()'>▼</button>";
+}
+function s13(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "あ、ひまわりたん！";
+	botan.innerHTML = "<button type=button onclick='s14()'>▼</button>";
+}
+function s14(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "ジャックがどうかしたの？";
+	botan.innerHTML = "<button type=button onclick='s15()'>▼</button>";
+}
+function s15(){
+	chara.innerHTML = "ひまわりたん";
+	document.mes.message.value = "ちょっと買い物に行ってきてほしいんだ";
+	botan.innerHTML = "<button type=button onclick='s16()'>▼</button>";
+}
+function s16(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "ん？　買い物？　全然いいよ！";
+	botan.innerHTML = "<button type=button onclick='s17()'>▼</button>";
+}
+function s17(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "あ、じゃあお菓子買ってきて！";
+	botan.innerHTML = "<button type=button onclick='s18()'>▼</button>";
+}
+function s18(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "……生きる木から予算は降りるんだよね？";
+	botan.innerHTML = "<button type=button onclick='s19()'>▼</button>";
+}
+function s19(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "！(ギクッ)";
+	botan.innerHTML = "<button type=button onclick='s20()'>▼</button>";
+}
+function s20(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "ひ、ひまわりた～ん……";
+	botan.innerHTML = "<button type=button onclick='s21()'>▼</button>";
+}
+function s21(){
+	chara.innerHTML = "ひまわりたん";
+	document.mes.message.value = "お金は、当然生きる木が払うんだよ";
+	botan.innerHTML = "<button type=button onclick='s22()'>▼</button>";
+}
+function s22(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "今、お金ないのに……";
+	botan.innerHTML = "<button type=button onclick='s23()'>▼</button>";
+}
+function s23(){
+	chara.innerHTML = "ジャック";
+	document.mes.message.value = "じゃあ行ってきま～す！";
+	botan.innerHTML = "<button type=button onclick='s24()'>▼</button>";
+}
+function s24(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "ひまわりた～ん！　ジャックを止めて～！";
+	botan.innerHTML = "<button type=button onclick='s25()'>▼</button>";
+}
+function s25(){
+	chara.innerHTML = "ひまわりたん";
+	document.mes.message.value = "止められないよ。こっちも買い物頼んでるんだから……";
+	botan.innerHTML = "<button type=button onclick='s26()'>▼</button>";
+}
+function s26(){
+	chara.innerHTML = "生きる木";
+	document.mes.message.value = "うぅ……";
+	botan.innerHTML = "<button type=button onclick='s27()'>▼</button>";
+}
+function s27(){
+	chara.innerHTML = "～おしまい～";
+	document.mes.message.value = "Novel-DEV v0.9.1\n\n(C)なつ";
+	botan.innerHTML = "<button type=button onclick='s27()'>現在絶賛開発中</button>";
+}
