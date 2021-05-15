51cafd1e57e14078059bd60e7f265f28daa00281
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-05-15 19:05:35 +0900

    v2.0 (big update!!)
    AppBASE及び、ジャックと生きる木の最新ゲームファイルを追加しました！
    現在テスト段階ですが、GitHubを利用してテストを行うことができます。

---
+++ html/grow_flowers/cookie.js
@@ -0,0 +1,3 @@
+function kukki1() {
+document.cookie = 'return=1; expires=Thu, 1-Jan-2030 00:00:00 GMT;';
+}
---
+++ html/grow_flowers/ohana.php
@@ -0,0 +1,7 @@
+<?php
+echo 'phpDESU.';
+echo '</br>';
+echo """<a href="javascript:location.href = '/grow_flowers/mian'">"""
+
+
+?>
